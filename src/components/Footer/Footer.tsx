927c8068467a8bc4305ed2bf60cbba983026b97f
dmitrygvl <dmitrygv1994@ya.ru>
2024-01-28 13:22:29 +0300

    Add Header & Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Footer: FC = () => {
+  return (
+    <footer className="relative overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black">
+      <p>© 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
